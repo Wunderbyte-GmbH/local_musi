fe81456afa6d50d31e0db48dde9cdc2bce44db67
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2025-06-24 16:23:25 +0200

    Version 0.9.21 (2025062400)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_musi';
-$plugin->release = '0.9.20';
-$plugin->version = 2025061100;
+$plugin->release = '0.9.21';
+$plugin->version = 2025062400;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
