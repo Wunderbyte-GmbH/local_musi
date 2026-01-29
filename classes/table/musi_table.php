216e6ecdd498f5542af1e10cd755cf6dcb95af55
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2026-01-29 16:24:10 +0100

    Bugfix: Use arg_is_true helper function instead of !empty check.

--- classes/table/musi_table.php
+++ classes/table/musi_table.php
@@ -28,6 +28,7 @@ use mod_booking\option\dates_handler;
 use mod_booking\output\col_availableplaces;
 use mod_booking\output\col_teacher;
 use mod_booking\price;
+use mod_booking\shortcodes_handler;
 use mod_booking\singleton_service;
 use moodle_exception;
 use moodle_url;
@@ -273,7 +274,7 @@ class musi_table extends wunderbyte_table {
             $bacache->{$bakey}[$user->id]['expirationtime'] = time() + $expirationseconds;
             $cache->set($cachekey, $bacache);
         }
-        if (isset($this->displayoptions['additionalpricetext']) && !empty($this->displayoptions['additionalpricetext'])) {
+        if (shortcodes_handler::arg_is_true($this->displayoptions['additionalpricetext'] ?? false)) {
             $additionalpricetext = get_string('additionalpricetext', 'local_musi');
         } else {
             $additionalpricetext = '';
